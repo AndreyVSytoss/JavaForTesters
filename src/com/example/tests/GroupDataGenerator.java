af041ca30b609f84fa5bd9869ecea646399a3588
Laptop <hefopmat@inbox.ru>
2015-07-25 00:01:06 +0300

    Add new changes from lesson 5

--- src/com/example/tests/GroupDataGenerator.java
+++ src/com/example/tests/GroupDataGenerator.java
@@ -10,6 +10,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import com.thoughtworks.xstream.XStream;
+
 public class GroupDataGenerator {
 
 	public static void main(String[] args) throws IOException {
@@ -38,9 +40,19 @@ public class GroupDataGenerator {
 		}
 	}
 
-	private static void saveGroupsToXmlFile(List<GroupData> groups, File file) {
-		// TODO Auto-generated method stub
-		
+	private static void saveGroupsToXmlFile(List<GroupData> groups, File file) throws IOException {
+		XStream xstream = new XStream();
+		xstream.alias("group", GroupData.class);
+		String xml = xstream.toXML(groups);
+		FileWriter writer = new FileWriter(file);
+		writer.write(xml);
+		writer.close();
+	}
+	
+	public static List<GroupData> loadGroupsFromXmlFile(File file) {
+		XStream xstream = new XStream();
+		xstream.alias("group", GroupData.class);
+		return (List<GroupData>) xstream.fromXML(file);	
 	}
 
 	private static void saveGroupsToCsvFile(List<GroupData> groups, File file) throws IOException {
