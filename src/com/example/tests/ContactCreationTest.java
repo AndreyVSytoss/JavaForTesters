ea447b9d22f985980a5e9b47d0a36bbd4ae93547
Laptop <hefopmat@inbox.ru>
2015-07-31 22:04:59 +0300

    final 5.1

--- src/com/example/tests/ContactCreationTest.java
+++ src/com/example/tests/ContactCreationTest.java
@@ -1,13 +1,14 @@
 package com.example.tests;
 
 import static com.example.tests.ConctactDataGenerator.generateRandomContacts;
-import static com.example.tests.ConctactDataGenerator.loadContactsFromFile;
+import static com.example.tests.ConctactDataGenerator.loadContactsFromXmlFile;
+import static com.example.tests.ConctactDataGenerator.loadContactsFromCsvFile;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Iterator;
-import java.util.List;
 
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -17,8 +18,8 @@ import com.example.utils.SortedListOf;
 public class ContactCreationTest extends TestBase {
 	
 	@DataProvider
-	public Iterator<Object[]> contactsFromFile() {
-		return wrapContactsForDataProvider(loadContactsFromFile(new File("contacts.txt"))).iterator();
+	public Iterator<Object[]> contactsFromFile() throws IOException {
+		return wrapContactsForDataProvider(loadContactsFromXmlFile(new File("contacts.xml"))).iterator();
 	}
 	
 	@Test(dataProvider = "contactsFromFile")
