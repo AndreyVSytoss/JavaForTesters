83c79515f5b67d1df2d37c555a5ffe6faaccb198
Laptop <hefopmat@inbox.ru>
2015-08-06 00:14:40 +0300

    last commit

--- src/com/example/tests/TestBase.java
+++ src/com/example/tests/TestBase.java
@@ -17,6 +17,7 @@ import static com.example.tests.ConctactDataGenerator.generateRandomContacts;
 
 
 
+
 import com.example.fw.ApplicationManager;
 
 
@@ -26,8 +27,9 @@ public class TestBase {
 	
 	@BeforeTest
 	public void setUp() throws Exception {
+		String configProperty = System.getProperty("configFile", "application.properties");
 		Properties properties = new Properties();
-		properties.load(new FileReader(new File("application.properties")));
+		properties.load(new FileReader(new File("configFile")));
 		app = new ApplicationManager(properties);
 		}
 
